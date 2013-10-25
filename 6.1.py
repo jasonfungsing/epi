9f87052e750ac7cd6e228ab00111db94f47a9750
Jason Fung <jasonfungsing@gmail.com>
2013-10-25 21:12:37 +1100

    added print into 6.1

--- 6.1.py
+++ 6.1.py
@@ -1,5 +1,6 @@
 __author__ = 'jasonfungsing'
 
+
 def dutchNationalFlag(inputArray, pivotIndex):
     size = len(inputArray)
     small = 0
@@ -7,22 +8,25 @@ def dutchNationalFlag(inputArray, pivotIndex):
     large = size - 1
 
     pivot = inputArray[pivotIndex]
+    print("the pivot is: " + str(pivot))
     while equal < large:
-        print("now the array is: " + str(inputArray))
-        if inputArray[equal] < pivot:
+        current = inputArray[equal]
+        print("the array is: " + str(inputArray))
+        print("start process: " + str(current))
+        if current < pivot:
             inputArray[small], inputArray[equal] = inputArray[equal], inputArray[small]
             small += 1
             equal += 1
-        elif inputArray[equal] == pivot:
+        elif current == pivot:
             equal += 1
         else:
             inputArray[large], inputArray[equal] = inputArray[equal], inputArray[large]
             large -= 1
+        print("after process " + str(current) + ",  now array is: " + str(inputArray))
 
     return inputArray
 
 
-
-print(dutchNationalFlag([2,4,3,1,6,5,7], 1))
+print(dutchNationalFlag([2, 4, 3, 1, 6, 5, 7], 1))
 
 
